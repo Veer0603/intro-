898db9753cb66d51fc323e2fb110c995b28e0a6e
Veer0603 <vamehta123@gmail.com>
2025-01-27 22:31:57 +0530

    Add files via upload

---
+++ casealphabets.py
@@ -0,0 +1,11 @@
+def uppercase():
+    for i in range(65,91):
+        print(chr(i),end=" ")
+def lowercase():
+    for i in range(97,123):
+        print(chr(i),end=" ")        
+print("The lower case alphabets are : ")
+lowercase();
+print()
+print("The lower case alphabets are : ")
+uppercase();
